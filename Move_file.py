369d58fba10e548153c1f356a449143b01d0ef3f
mvandita <86829958+mvandita@users.noreply.github.com>
2023-06-09 20:46:44 +0530

    Add files via upload

---
+++ Move_file.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+from_dir ="C:/Users/Admin"
+to_dir ="C:/Users/Admin/Desktop"
+
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+
+
+list_of_files = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.xlsx' '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+
+        name, extension = os.path.splitext(event.src_path)
+       
+        time.sleep(1)
+
+        for key, value in list_of_files.items():
+            time.sleep(1)
+
+
+if os.path.exists(path2):
+    print("Moving"+file_name+"  ")
+
+    shutil.move(path1,path3)
+
+else:
+    os.makedirs(path2)
+    print("Moving"+file_name+"  ")
+    shutil.move(path1,path3)
